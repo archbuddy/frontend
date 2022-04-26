8e2c6a6a6766382d141385009f1f41b0151cd057
Bruno Penso <bruno.penso@gmail.com>
2022-04-25 21:48:11 -0300

    wrapper

--- src/nodes/SystemNode.js
+++ src/nodes/SystemNode.js
@@ -4,22 +4,14 @@ import { Handle, Position } from 'react-flow-renderer';
 // https://github.com/wbkd/react-flow/issues/1641
 
 function SystemNode({ data, isConnectable }) {
-  const changeStyleOver = () => {
-    console.log('over')
-  }
-  const changeStyleOut = () => {
-    console.log('out')
-  }
   return (
-    <>
-      <Handle position={Position.Top} id='a1' key='a1' type="source" className="handleSystemNodeSource"/>
-      <Handle position={Position.Right} id="r1" key='r1' type="source" className="handleSystemNodeSource"/>
-      <Handle position={Position.Bottom} id="b1" key='b1' type="target" className="handleSystemNodeTarget"/>
-      <Handle position={Position.Left} id="l1" key='l1' type="target" className="handleSystemNodeTarget"/>
-      <div className='systemNode' onMouseOver={changeStyleOver} onMouseOut={changeStyleOut}>
+    <div className='systemNode'>
+      <Handle position={Position.Top} id='a1' key='a1' type="source" className="handleSystemNode handleSystemNodeSource"/>
+      <Handle position={Position.Right} id="r1" key='r1' type="source" className="handleSystemNode handleSystemNodeSource"/>
+      <Handle position={Position.Bottom} id="b1" key='b1' type="target" className="handleSystemNode handleSystemNodeTarget"/>
+      <Handle position={Position.Left} id="l1" key='l1' type="target" className="handleSystemNode handleSystemNodeTarget handleSystemNodeLeft"/>
         {data.label}
-      </div>
-    </>
+    </div>
   );
 }
 
