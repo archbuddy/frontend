0a153c573fd23ea68ac3a2a403e5367946ca2f91
Bruno Penso <bruno.penso@gmail.com>
2022-04-27 21:41:39 -0300

    remove hardcoded

--- src/tmp/edges.js
+++
@@ -1,5 +0,0 @@
-export default [
-    { id: 'n1n2', source: 'n1', sourceHandle: 'r1', target: 'n2', targetHandle: 'l1', label: 'authenticate' },
-    { id: 'n1n3', source: 'n1', sourceHandle: 'r1', target: 'n3', targetHandle: 'l1', label: 'authenticate' },
-  ];
-  
