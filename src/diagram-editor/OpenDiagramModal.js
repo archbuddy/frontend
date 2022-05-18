2fe02c181e3857fc9c15bb1935842efcf61bca32
Francisco Ribas <chico.bribas@gmail.com>
2022-05-17 22:30:08 -0300

    Open diagram modal
    Signed-off-by: Francisco Ribas <chico.bribas@gmail.com>

--- src/diagram-editor/OpenDiagramModal.js
+++ src/diagram-editor/OpenDiagramModal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Modal,
   ModalOverlay,
@@ -9,15 +9,28 @@ import {
   ModalFooter,
   Button
 } from '@chakra-ui/react'
+import srvViewPoint from '../services/viewpoint'
 
 export default function OpenDiagramModal(props) {
+  const [viewPoints, setViewPoints] = useState([])
+
+  useEffect(() => {
+    const loadViewPoints = async () => {
+      setViewPoints(await srvViewPoint.list())
+    }
+
+    if (props.isOpen) {
+      loadViewPoints()
+    }
+  }, [props.isOpen])
+
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Modal Title</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>the body</ModalBody>
+        <ModalBody>{viewPoints.map((v) => v.name)}</ModalBody>
 
         <ModalFooter>
           <Button colorScheme="blue" mr={3} onClick={props.onClose}>
