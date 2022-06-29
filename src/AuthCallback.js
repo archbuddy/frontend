d35770bdbbfeb84613df5c9637dea5e371c1477d
Bruno Penso <bruno.penso@gmail.com>
2022-06-28 23:15:33 -0300

    rename

--- src/AuthCallback.js
+++ src/AuthCallback.js
@@ -1,34 +1,34 @@
 // check this for default edges configs https://reactflow.dev/docs/api/edges/edge-types/
 
 import React, { useEffect, useState } from 'react'
-import { ChakraProvider, Flex, Spacer, Text, Spinner, Box } from '@chakra-ui/react'
+import { ChakraProvider, Flex, Spacer, Text, Spinner, Box, Button } from '@chakra-ui/react'
 import { useLocation, useSearchParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
-
 import Header from './Header'
 import Footer from './Footer'
-import Authentication from './services/auth'
+import srvAuth from './services/auth'
 import { log } from './util'
 
-function PageNotFound() {
-  // eslint-disable-next-line no-unused-vars
+function AuthCallback() {
+  let location = useLocation()
   let navigate = useNavigate()
   const [hasJwt, setHasJwt] = useState(false)
-
-  let location = useLocation()
   // eslint-disable-next-line no-unused-vars
   let [search, setSearch] = useSearchParams()
 
   useEffect(() => {
-    process(search.get('type'), location.hash)
-  }, [search, location])
+    async function load() {
+      const data = await srvAuth.authenticate(search.get('type'), location.hash)
+      log('setting jwt token' + data.token)
+      localStorage.setItem('jwt', data.token)
+      log('ready to navigate authenticated, redirect')
+      setHasJwt(true)
+    }
+    load()
+  }, [])
 
-  const process = async (type, params) => {
-    const data = await Authentication.authenticate(type, params)
-    log('setting jwt token')
-    localStorage.setItem('jwt', data.token)
-    log('ready to navigate authenticated, redirect')
-    setHasJwt(true)
+  const redirect = () => {
+    navigate('/diagram', { replace: true })
   }
 
   return (
@@ -38,8 +38,8 @@ function PageNotFound() {
         <Spacer>
           <Box style={{ padding: '10px' }}>
             <Spinner color="blue.500" />
-            <Text>Carregando...</Text>
-            {hasJwt ? navigate('/diagram', { replace: true }) : <></>}
+            <Text>Loading...</Text>
+            {hasJwt ? <Button onClick={redirect}>Continue</Button> : <></>}
           </Box>
         </Spacer>
         <Footer />
@@ -48,4 +48,4 @@ function PageNotFound() {
   )
 }
 
-export default PageNotFound
+export default AuthCallback
