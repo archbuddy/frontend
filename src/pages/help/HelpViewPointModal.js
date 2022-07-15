3dc4f8d2f47979ed7f88b3b56470e5d799635b3e
Bruno Penso <bruno.penso@gmail.com>
2022-07-15 10:50:21 -0300

    help text

--- src/pages/help/HelpViewPointModal.js
+++ src/pages/help/HelpViewPointModal.js
@@ -8,9 +8,12 @@ import {
   ModalBody,
   ModalFooter,
   Button,
-  Text,
-  Box
+  List,
+  ListItem,
+  ListIcon
 } from '@chakra-ui/react'
+import { AiFillFolderOpen } from 'react-icons/ai'
+import { MdCheckCircle } from 'react-icons/md'
 
 export default function HelpViewPointModal(props) {
   const close = () => {
@@ -18,17 +21,40 @@ export default function HelpViewPointModal(props) {
   }
 
   return (
-    <Modal isOpen={props.isOpen} onClose={close}>
+    <Modal isOpen={props.isOpen} onClose={close} size="lg">
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Help</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Box>
-            <Text>help text</Text>
-          </Box>
+          <List>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color="blue.500" />
+              To create or select a View Point click on <AiFillFolderOpen />
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color="blue.500" />
+              To add new object to the view point just select on the left and drag to the center of
+              the canvas
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color="blue.500" />
+              To edit a already in object double click on it. You will be able to change some
+              attributes and remove it from this view point
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color="blue.500" />
+              To connect to objects just point your mouse to the little grey dots on the object and
+              the mouse wil turn in a plus sign, after that click and drag to the other point where
+              you want to connect
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color="blue.500" />
+              After connecting to objects a line is displayed you can click and edit the
+              information, to create new connections just repeat the connection process
+            </ListItem>
+          </List>
         </ModalBody>
-
         <ModalFooter>
           <Button onClick={close}>Close</Button>
         </ModalFooter>
