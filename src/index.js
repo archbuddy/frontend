42c74ed6708fafceff3953884c808e7476cf7a59
Bruno Penso <bruno.penso@gmail.com>
2022-07-19 21:59:36 -0300

    add GA4

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './AuthContext'
 import App from './App'
+import ReactGA from 'react-ga4'
+const TRACKING_ID = '323890448'
+ReactGA.initialize(TRACKING_ID)
 
 const rootElement = document.getElementById('root')
 
